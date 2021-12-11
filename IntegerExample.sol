c6e7624d62fef3329d6333f1b16fa6c9ee5b531d
Paul Touchton <paultouchton@gmail.com>
2021-12-11 13:29:35 -0800

    some contracts

---
+++ IntegerExample.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.1;
+
+contract IntegerExample {
+    uint public myUnit;
+
+    function setMyUnit(uint _myUnit) public {
+        myUnit = _myUnit;
+    }
+}
